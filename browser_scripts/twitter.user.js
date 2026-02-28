0ae10e0097aef80d6d2660b3fc70ffd73e0a0162
Neo Kai Neng <neo.kai.neng@gmail.com>
2026-02-28 11:06:02 +0800

    add enable group folder

--- browser_scripts/twitter.user.js
+++ browser_scripts/twitter.user.js
@@ -15,7 +15,7 @@
 (function() {
     'use strict';
     window.addEventListener('scroll', function() {
-        let format = '.jpeg'; // can change to .png or .jpg
+        let enableGroupFolder = true;
         let arts = Array.from(document.querySelectorAll('article'));
         for(let art of arts) {
             if(art.getAttribute('data-index')) continue;
@@ -29,46 +29,23 @@
                 solo.style.fontSize = '1.2rem';
                 solo.className = 'monkey-action-solo';
                 solo.innerText = '👤';
-                solo.onclick = function() {
-                    event.preventDefault();
-                    let img = event.target.parentElement.querySelector('img');
-                    let imgUrl = new URL(img.src);
-                    imgUrl.searchParams.set("name", "large");
-                    let fullSizeImg = imgUrl.toString();
-                    let source = fullSizeImg.slice(fullSizeImg.indexOf('/media/')+7, fullSizeImg.indexOf('?')) + format;
-                    let art = event.target.closest('article');
-                    let user = art.querySelector('[data-testid="User-Name"]').innerText;
-                    let folder = mapper(user);
-                    if(!folder) {
-                        let result = prompt('mapping not found! enter mapping in format [handle]/[folder] or as [folder] for one-time add');
-                        if(result) {
-                            let sections = result.split('/');
-                            if(sections.length == 2) {
-                                list[sections[0]] = sections[1];
-                                localStorage.setItem('x-monkey-action-mapper', JSON.stringify(list));
-                                user = sections[0];
-                                folder = mapper(user);
-                            }
-                            else if(sections.length == 1) {
-                                folder = sections[0];
-                            }
-                            else {
-                                return alert('mapping add failed! try again');
-                            }
-                        }
-                        else {
-                            return alert('mapping add failed! try again');
-                        }
-                    }
-                    let url = 'dlapp://save?url={link}&name={filename}&user={user}'
-                    .replace('{link}', encodeURIComponent(fullSizeImg))
-                    .replace('{filename}', encodeURIComponent(source))
-                    .replace('{user}', encodeURIComponent(folder));
-                    console.log(url);
-                    window.location.href = url;
-                };
+                solo.onclick = downloader;
                 if(!art.querySelector('.monkey-action-solo')) {
                     menu.parentElement.appendChild(solo);
+                }
+                if(enableGroupFolder) {
+                    let grp = art.querySelector('.monkey-action-grp') || document.createElement('button');
+                    grp.style.cursor = 'pointer';
+                    grp.style.position = 'absolute';
+                    grp.style.top = '1.2rem';
+                    grp.style.zIndex = '9';
+                    grp.style.fontSize = '1.2rem';
+                    grp.className = 'monkey-action-grp';
+                    grp.innerText = '👥';
+                    grp.onclick = downloader;
+                    if(!art.querySelector('.monkey-action-grp')) {
+                        menu.parentElement.appendChild(grp);
+                    }
                 }
             }
         }
@@ -88,6 +65,48 @@ var mapper = function(data) {
     return null;
 };
 
+var downloader = function(event) {
+    event.preventDefault();
+    let format = '.jpeg'; // can change to .png or .jpg
+    let img = event.target.parentElement.querySelector('img');
+    let imgUrl = new URL(img.src);
+    imgUrl.searchParams.set('name', 'large');
+    let fullSizeImg = imgUrl.toString();
+    let source = fullSizeImg.slice(fullSizeImg.indexOf('/media/')+7, fullSizeImg.indexOf('?')) + format;
+    let art = event.target.closest('article');
+    let user = art.querySelector('[data-testid="User-Name"]').innerText;
+    let folder = mapper(user);
+    if(!folder) {
+        let result = prompt('mapping not found! enter mapping in format [handle]/[folder] or as [folder] for one-time add');
+        if(result) {
+            let sections = result.split('/');
+            if(sections.length == 2) {
+                list[sections[0]] = sections[1];
+                localStorage.setItem('x-monkey-action-mapper', JSON.stringify(list));
+                user = sections[0];
+                folder = mapper(user);
+            }
+            else if(sections.length == 1) {
+                folder = sections[0];
+            }
+            else {
+                return alert('mapping add failed! try again');
+            }
+        }
+        else {
+            return alert('mapping add failed! try again');
+        }
+    }
+    if(event.target.classList.contains('monkey-action-grp')) {
+        folder = '';
+    }
+    let url = 'dlapp://save?url={link}&name={filename}&user={user}'
+    .replace('{link}', encodeURIComponent(fullSizeImg))
+    .replace('{filename}', encodeURIComponent(source))
+    .replace('{user}', encodeURIComponent(folder));
+    console.log(url);
+    window.location.href = url;
+};
+
 // add your own mappings here to prevent reliance on browser storage
 var list = {};
-
